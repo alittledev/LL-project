99e77649fb9097d94de483a5721d1810a8b61f71
A Little <53057083+alittledev@users.noreply.github.com>
2022-11-23 15:02:52 -0600

    Map
    added google map

--- scripts.js
+++ scripts.js
@@ -28,3 +28,25 @@ $(document).ready(function(){
         ]
     });
   });
+
+function initMap() {
+ // The location of Lifted Logic
+ const liftedLogic = { lat: 38.9872497, lng: -94.6684171};
+ // The map, centered at Lifted Logic
+ const map = new google.maps.Map(document.getElementById("map"), {
+     mapId: "d4122c77b5410b5b",
+   zoom: 12,
+   center: liftedLogic,
+ });
+ // The marker, positioned at Lifted Logic
+ const marker = new google.maps.Marker({
+   position: liftedLogic,
+   map: map,
+ });
+}
+
+window.initMap = initMap;
+
+
+
+  
