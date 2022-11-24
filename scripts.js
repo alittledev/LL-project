d64de0f40c9fe60f66e1e1a409399020dd177a17
A Little <53057083+alittledev@users.noreply.github.com>
2022-11-24 11:56:43 -0600

    Contact & footer
    get in touch and footer sections

--- scripts.js
+++ scripts.js
@@ -34,8 +34,8 @@ function initMap() {
  const liftedLogic = { lat: 38.9872497, lng: -94.6684171};
  // The map, centered at Lifted Logic
  const map = new google.maps.Map(document.getElementById("map"), {
-     mapId: "d4122c77b5410b5b",
-   zoom: 12,
+   mapId: "d4122c77b5410b5b",
+   zoom: 14,
    center: liftedLogic,
  });
  // The marker, positioned at Lifted Logic
